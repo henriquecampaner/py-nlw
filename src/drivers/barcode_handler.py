ebfa49b5b18399b8c4e43920cbb8a444250e42db
Henrique <henrique.campaner@live.com>
2024-02-06 20:00:37 +0000

    Refactor barcode image saving path

--- src/drivers/barcode_handler.py
+++ src/drivers/barcode_handler.py
@@ -1,3 +1,5 @@
+import os
+
 from barcode import Code128
 from barcode.writer import ImageWriter
 
@@ -5,7 +7,8 @@ from barcode.writer import ImageWriter
 class BarcodeHandler:
     def create_barcode(self, product_code: str) -> str:
         tag = Code128(product_code, writer=ImageWriter())
-        path_from_tag = f"{tag}x"
-        tag.save(path_from_tag)
+        path_from_tag = f"{tag}"
+        images_folder = "src/views/static/images"
+        tag.save(os.path.join(images_folder, path_from_tag))
 
         return path_from_tag
